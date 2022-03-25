6cb8f69e951dbd777e44e7a984b738365855cc5a
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-25 12:27:02 -0400

    update

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,16 +1,25 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_MEcd= gql`
+export const QUERY_ME= gql`
   {
     me {
       _id
       username
       email
-      friendCount
-      friends {
-        _id
-        username
+      bookCount
+      savedBooks {
+        bookId
+        title
+        link
       }
     }
   }
 `;
+
+export const QUERY_USERS= gql`
+  query users { 
+   username
+   email
+  }
+`;
+
--- server/server.js
+++ server/server.js
@@ -25,15 +25,20 @@ const startServer = async () => {
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
 
+  // Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
   // log where we can go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
 
 
 // Initialize the Apollo server
@@ -42,14 +47,8 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
